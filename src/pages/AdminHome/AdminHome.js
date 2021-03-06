20fa442b8ae26ee061a5e91bc0f3cd7c902c0a1c
Cooper Saye <coopersaye@gmail.com>
2021-03-05 20:47:38 -0700

    Create admin form.

--- src/pages/AdminHome/AdminHome.js
+++ src/pages/AdminHome/AdminHome.js
@@ -1,7 +1,36 @@
+import React, { useState } from 'react';
+import { IonButton, IonInput, IonItem, IonLabel } from '@ionic/react';
+
+import './AdminHome.css';
+
+import firebase from 'firebase/app';
+
 function AdminHome() {
+  function createWorkout() {
+
+  }
+
   return (
-    <div className="AdminHome">
-      <p>Admin home</p>
+    <div className="AdminHome center-box">
+      <form onSubmit={createWorkout}>
+        <div className="input-section">
+          <h1 className="input-title">Workout Title</h1>
+          <IonInput
+          className="input-item"
+          required
+          />
+        </div>
+        <div className="margin-sm"></div>
+        <div className="input-section">
+          <h1 className="input-title">Workout Date</h1>
+          <IonInput
+          type="date"
+          className="input-item"
+          required
+          />
+        </div>
+        <IonButton type="submit" className="submit-button">Create Workout</IonButton>
+      </form>
     </div>
   );
 }
