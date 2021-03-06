ce2e1ad4b29150b74cbe8c446d2f4709a238d884
Cooper Saye <coopersaye@gmail.com>
2021-03-05 21:09:30 -0700

    Create student workout list.

--- src/pages/StudentHome/StudentHome.js
+++ src/pages/StudentHome/StudentHome.js
@@ -1,7 +1,64 @@
+import React, { useState } from 'react';
+import { IonSelect, IonSelectOption, IonItem, IonLabel } from '@ionic/react';
+
+import firebase from 'firebase/app';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 function StudentHome() {
+  const [group, setGroup] = useState('');
+
+  // get groups from firebase
+  const groupsQuery = firebase.firestore().collection('groups').orderBy('name');
+  const [groups] = useCollectionData(groupsQuery);
+
+  const workoutsQuery = firebase.firestore().collection('workouts')
+  .where('groupId', '==', group ? group : 'null');
+  const [workouts] = useCollectionData(workoutsQuery, {idField: 'id'});
+
+  if (!groups) {
+    return (
+      <div className="StudentHome">
+        <p className="margin-sm">Loading...</p>
+      </div>
+    )
+  }
+
   return (
-    <div className="StudentHome">
-      <p>Student home</p>
+    <div className="StudentHome center-box">
+      <IonItem>
+        <IonLabel>Select Group</IonLabel>
+        <IonSelect value={group} onIonChange={e => setGroup(e.target.value)}>
+        {
+          groups.map(g =>
+            <IonSelectOption key={g.id} value={g.id}>{g.name}</IonSelectOption>
+          )
+        }
+        </IonSelect>
+      </IonItem>
+      {
+        group &&
+        <div className="workouts-list">
+        {
+          workouts ?
+          <>
+            {
+              workouts.length > 0 ?
+              <>
+                {
+                  workouts.map(w =>
+                    <div>
+                      {w.title}
+                    </div>
+                  )
+                }
+              </> :
+              <p>No workouts yet</p>
+            }
+          </> :
+          <p className="margin-sm">Loading workouts...</p>
+        }
+      </div>
+      }
     </div>
   );
 }
