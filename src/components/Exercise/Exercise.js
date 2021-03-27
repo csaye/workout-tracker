c26fb20775e2225346d3cf637dffbc305987aece
Cooper Saye <coopersaye@gmail.com>
2021-03-27 17:45:44 -0600

    Implement user comments on exercises.

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,10 +1,30 @@
+// general imports
+import React, { useState } from 'react';
+import { IonButton } from '@ionic/react';
+import firebase from 'firebase/app';
+
 // style
 import './Exercise.css';
 
 // Exercise component
 function Exercise(props) {
+  const [studentComments, setStudentComments] = useState('');
+
   // get exercise data from props
-  const { name, sets, reps, comments } = props.data;
+  const { id, name, sets, reps, comments } = props.data;
+
+  const studentUid = firebase.auth().currentUser.uid;
+  const studentName = firebase.auth().currentUser.displayName;
+
+  // saves comments for current exercise
+  function saveComments(e) {
+    e.preventDefault();
+    // update firestore with comments
+    firebase.firestore().collection('exercises').doc(id).collection('comments').doc(studentUid).set({
+      name: studentName,
+      comments: studentComments
+    });
+  }
 
   return (
     <div className="Exercise soft-shadow">
@@ -14,6 +34,10 @@ function Exercise(props) {
         <p>Reps: {reps}</p>
       </div>
       <p><i>{comments}</i></p>
+      <form onSubmit={saveComments}>
+        <input placeholder="How did it feel?" value={studentComments} onChange={e => setStudentComments(e.target.value)} required />
+        <IonButton type="submit">Save Comment</IonButton>
+      </form>
     </div>
   );
 }
