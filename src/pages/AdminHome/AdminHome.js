df906306175a7fa1eb5875a892cc56aa9301b5ff
Cooper Saye <coopersaye@gmail.com>
2021-03-26 19:13:04 -0600

    Implement all workout viewing.

--- src/pages/AdminHome/AdminHome.js
+++ src/pages/AdminHome/AdminHome.js
@@ -104,130 +104,139 @@ function AdminHome() {
   return (
     <div className="AdminHome center-box">
       {
-        // if current workout set, return editing page
-        workout ?
-        <>
-          <h1 className="edit-workout">Editing "{workout.title}"</h1>
-          {/* create exercise form */}
-          <form onSubmit={createExercise} className="input-section">
-            {/* name input */}
-            <h4 className="input-title">Exercise Name</h4>
-            <IonInput
-            value={name}
-            onIonChange={e => setName(e.target.value)}
-            className="input-item"
-            placeholder="exercise name"
-            required
-            />
-            {/* sets input */}
-            <h4 className="input-title">Sets</h4>
-            <IonInput
-            value={sets}
-            type="number"
-            onIonChange={e => setSets(e.target.value)}
-            step="1"
-            min="1"
-            className="input-item"
-            required
-            />
-            {/* reps input */}
-            <h4 className="input-title">Reps</h4>
-            <IonInput
-            value={reps}
-            type="number"
-            onIonChange={e => setReps(e.target.value)}
-            step="1"
-            min="1"
-            className="input-item"
-            required
-            />
-            {/* comments input */}
-            <h4 className="input-title">Comments</h4>
-            <IonInput
-            value={comments}
-            onIonChange={e => setComments(e.target.value)}
-            className="input-item"
-            placeholder="comments"
-            />
-            <IonButton className="create-workout hover-scale" type="submit">Create Exercise</IonButton>
-          </form>
+      {
+        groupId &&
+        <div>
           {
-            // map exercises to AdminExercise components
-            exercises ?
-            exercises.map(e => <AdminExercise key={e.id} data={e} />) :
-            // show loading if no exercises yet
-            <p>Loading exercises...</p>
-          }
-          {
-            // if workout
+            // if current workout set, return editing page
             workout ?
             <>
+              <h1 className="edit-workout">Editing "{workout.title}"</h1>
+              {/* create exercise form */}
+              <form onSubmit={createExercise} className="input-section">
+                {/* name input */}
+                <h4 className="input-title">Exercise Name</h4>
+                <IonInput
+                value={name}
+                onIonChange={e => setName(e.target.value)}
+                className="input-item"
+                placeholder="exercise name"
+                required
+                />
+                {/* sets input */}
+                <h4 className="input-title">Sets</h4>
+                <IonInput
+                value={sets}
+                type="number"
+                onIonChange={e => setSets(e.target.value)}
+                step="1"
+                min="1"
+                className="input-item"
+                required
+                />
+                {/* reps input */}
+                <h4 className="input-title">Reps</h4>
+                <IonInput
+                value={reps}
+                type="number"
+                onIonChange={e => setReps(e.target.value)}
+                step="1"
+                min="1"
+                className="input-item"
+                required
+                />
+                {/* comments input */}
+                <h4 className="input-title">Comments</h4>
+                <IonInput
+                value={comments}
+                onIonChange={e => setComments(e.target.value)}
+                className="input-item"
+                placeholder="comments"
+                />
+                <IonButton className="create-workout hover-scale" type="submit">Create Exercise</IonButton>
+              </form>
               {
-                workout.studentsComplete?.length > 0 ?
-                <p>Students complete: {workout.studentsComplete.join(', ')}</p> :
-                <p>No students have completed the workout yet</p>
+                // map exercises to AdminExercise components
+                exercises ?
+                exercises.map(e => <AdminExercise key={e.id} data={e} />) :
+                // show loading if no exercises yet
+                <p>Loading exercises...</p>
               }
+              {
+                // if workout
+                workout ?
+                <>
+                  {
+                    workout.studentsComplete?.length > 0 ?
+                    <p>Students complete: {workout.studentsComplete.join(', ')}</p> :
+                    <p>No students have completed the workout yet</p>
+                  }
+                </> :
+                <p>Loading students...</p>
+              }
+              <IonButton className="hover-scale right-margin" color="danger" onClick={deleteWorkout}>Delete Workout</IonButton>
+              <IonButton className="hover-scale" onClick={() => setWorkout('')}>Finish</IonButton>
             </> :
-            <p>Loading students...</p>
-          }
-          <IonButton className="hover-scale right-margin" color="danger" onClick={deleteWorkout}>Delete Workout</IonButton>
-          <IonButton className="hover-scale" onClick={() => setWorkout('')}>Finish</IonButton>
-        </> :
-        // if current workout not set, return create workout page
-        <>
-          <div className="input-section">
-            <h1 className="create-workout">Create a Workout</h1>
-            {/* create workout form */}
-            <form onSubmit={createWorkout}>
-              {/* group input */}
-              <IonItem className="group-select">
-                <IonLabel>Select Group</IonLabel>
-                <IonSelect value={groupId} onIonChange={e => setGroupId(e.target.value)} required>
-                {
-                  // map groups to select options
-                  groups.map(g =>
-                    <IonSelectOption key={g.id} value={g.id}>{g.name}</IonSelectOption>
-                  )
-                }
-                </IonSelect>
-              </IonItem>
-              {/* title input */}
-              <h4 className="input-title">Workout Title</h4>
-              <IonInput
-              value={title}
-              onIonChange={e => setTitle(e.target.value)}
-              placeholder="title"
-              className="input-item"
-              required
-              />
-              {/* date input */}
-              <h4 className="input-title">Workout Date</h4>
-              <IonInput
-              value={date}
-              onIonChange={e => setDate(e.target.value)}
-              type="date"
-              className="input-item"
-              required
-              />
-              <IonButton className="hover-scale create-workout" type="submit">Create Workout</IonButton>
-            </form>
-          </div>
-          {
-            // if workouts exist
-            workouts?.length > 0 &&
-            <div className="input-section top-margin">
-              <h1 className="edit-workout">Edit Workout</h1>
+            // if current workout not set, return create workout page
+            <>
+              <div className="input-section">
+                <h1 className="create-workout">Create a Workout</h1>
+                {/* create workout form */}
+                <form onSubmit={createWorkout}>
+                  {/* title input */}
+                  <h4 className="input-title">Workout Title</h4>
+                  <IonInput
+                  value={title}
+                  onIonChange={e => setTitle(e.target.value)}
+                  placeholder="title"
+                  className="input-item"
+                  required
+                  />
+                  {/* date input */}
+                  <h4 className="input-title">Workout Date</h4>
+                  <IonInput
+                  value={date}
+                  onIonChange={e => setDate(e.target.value)}
+                  type="date"
+                  className="input-item"
+                  required
+                  />
+                  <IonButton className="hover-scale create-workout" type="submit">Create Workout</IonButton>
+                </form>
+              </div>
               {
-                // map workouts to edit workout buttons
-                workouts.map(w =>
-                  <IonButton className="hover-scale edit-button" color="secondary" key={w.id} onClick={() => setWorkout(w)}>
-                    {w.title}
-                  </IonButton>
-                )
+                // if workouts exist
+                todayWorkouts?.length > 0 &&
+                <div className="input-section top-margin">
+                  <h1 className="edit-workout">Today's Workouts</h1>
+                  {
+                    // map workouts to edit workout buttons
+                    todayWorkouts.map(w =>
+                      <IonButton className="hover-scale edit-button" color="secondary" key={w.id} onClick={() => setWorkout(w)}>
+                        {w.title}
+                      </IonButton>
+                    )
+                  }
+                </div>
               }
-            </div>
+              {
+                // if workouts exist
+                workouts?.length > 0 &&
+                <div className="input-section top-margin">
+                  <h1 className="edit-workout">All Workouts</h1>
+                  {
+                    // map workouts to edit workout buttons
+                    workouts.map(w =>
+                      <IonButton className="hover-scale edit-button" color="secondary" key={w.id} onClick={() => setWorkout(w)}>
+                        {w.title}
+                      </IonButton>
+                    )
+                  }
+                </div>
+              }
+            </>
           }
-        </>
+        </div>
       }
     </div>
   );
